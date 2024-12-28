10f1f5bbaf641ed038fe80ba69c0cc6c05241c3b
Duyvd04 <vuducduy1112004@gmail.com>
2024-12-28 13:34:28 +0700

    Updatev1.2.2

--- src/main/java/com/example/chatapp/ChatAppApplication.java
+++ src/main/java/com/example/chatapp/ChatAppApplication.java
@@ -3,14 +3,9 @@ package com.example.chatapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * The main class for the Chat App application.
- */
 @SpringBootApplication
 public class ChatAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ChatAppApplication.class, args);
 	}
-
 }
