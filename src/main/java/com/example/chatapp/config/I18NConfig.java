b28124d53f169a0abbd6190c5c93222cebc55732
Duyvd04 <vuducduy1112004@gmail.com>
2024-12-29 09:31:42 +0700

    Updatev1.2.4: I18N

---
+++ src/main/java/com/example/chatapp/config/I18NConfig.java
@@ -0,0 +1,42 @@
+package com.example.chatapp.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.time.Duration;
+import java.util.Locale;
+
+@Configuration
+public class I18NConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver(){
+        CookieLocaleResolver clr = new CookieLocaleResolver();
+//        Locale locale = new Locale("vi");
+        clr.setDefaultLocale(Locale.ENGLISH);
+        clr.setCookieMaxAge(Duration.ofSeconds(3600));
+        return clr;
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+}
